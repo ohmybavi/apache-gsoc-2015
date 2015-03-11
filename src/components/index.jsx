ff9897989bc88e4b7c9049e644ea03b41da0f339
felizbear <imalyanov@gmail.com>
2015-03-11 23:02:36 +0900

    add all / remove all tag buttons

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,13 +1,24 @@
 import React from "react"
 import List from "./list.jsx"
 import Tags from "./tags.jsx"
-import {update} from "../util/getData.js"
+import {addLabel, removeLabel} from "../util/getData.js"
 
 export default React.createClass({
+  clickHandler(cb) {
+    return _ => Object.keys(this.props.labels).forEach(cb)
+  },
   render: function() {
     return (
       <div>
         <h1 className="top-header">Google Summer of Code</h1>
+        <label className="top-add-all"
+          onClick={this.clickHandler(addLabel)}>
+          Add all
+        </label>
+        <label className="top-remove-all"
+          onClick={this.clickHandler(removeLabel)}>
+          Remove all
+        </label>
         <Tags labels={this.props.labels} />
         <List data={this.props.data} />
       </div>
