88c9b21edafe1317a3608ebc5ff3a0ef100cecf0
felizbear <imalyanov@gmail.com>
2015-03-11 22:18:00 +0900

    separate tag into a component

---
+++ src/components/tag.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import {addLabel, removeLabel} from "../util/getData.js"
+
+export default React.createClass({
+  clickHandler(cb, x) {
+    return e => {
+      e.stopPropagation() 
+      cb(x)
+    }
+  },
+  render() {
+    const {label, active} = this.props
+    return (
+      <li className="tag" > 
+        <label className="tag-add"
+          onClick={this.clickHandler(addLabel, label)}>
+          {label}
+        </label>
+        <label className="tag-remove" 
+          onClick={this.clickHandler(removeLabel, label)}>
+          {active? " ×" : ""}
+        </label>
+      </li>
+    )
+  }
+})
--- src/components/tags.jsx
+++ src/components/tags.jsx
@@ -1,29 +1,13 @@
 import React from "react"
-import {addLabel, removeLabel} from "../util/getData.js"
+import Tag from "./tag.jsx"
 
 export default React.createClass({
-  tagClickHandler(x) {
-    return function(e) {
-      if (e.target.className == "tag-add") {
-        addLabel(x)      
-      }
-      else if (e.target.className == "tag-remove") {
-        removeLabel(x)  
-      }
-      // update(ys => ys.filter(y => y.labels.indexOf(x) > - 1))
-    }
-  },
-  render: function() {
-    const labels = Object.keys(this.props.labels)
+  render() {
+    const labelMap = this.props.labels
+    const labels = Object.keys(labelMap)
     return (
       <ul className="tags">
-        {labels.map(x => 
-          <li className="tag" onClick={this.tagClickHandler(x)}> 
-            <label className="tag-add">{x}</label>
-            <label className="tag-remove">
-              {this.props.labels[x]? " x" : ""}
-            </label>
-          </li>)}
+        {labels.map(x => <Tag label={x} active={labelMap[x]}/>)}
       </ul>
     )
   }
