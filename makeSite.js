b7f1eccaa0d6191945d97f06e2fc573981c7f56c
felizbear <imalyanov@gmail.com>
2015-03-18 18:48:47 +0900

    improve builder

--- makeSite.js
+++ makeSite.js
@@ -1,65 +1,78 @@
-var Nightmare = require("nightmare")
 var fs = require("fs-extra")
 var http = require("http")
-var hash = require("./hash.js")
-var resources = []
+var Nightmare = require("nightmare")
 
+var has = function(x, y) {return y.indexOf(x) > -1}
+var eq = function(x, y) {return x == y}
+
+var hash = require("./hash.js")
 var port = 8080
-var url = "http://localhost:" + port + "/"
+var hostname = "localhost"
+var host = "http://" + hostname + ":" + port + "/"
+var href = host + hash
+var downloaded = []
+var visited = [href]
 
 fs.existsSync("./dist") && fs.removeSync("./dist")
 fs.mkdirSync("./dist")
 
 module.exports = function(cb) {
 
-  return new Nightmare()
+  var nightmare = new Nightmare()
     .on("resourceReceived", function(response) {
-      if (resources.indexOf(response.url) == -1 && 
-          response.url != url &&
-          response.url != url + hash &&
-          response.url.indexOf(url) > -1) {
+      var url = response.url
+      
+      if (!has(url, downloaded) && !eq(url, href) && has(hostname, url)) {
         
-        resources.push(response.url)
+        downloaded.push(url)
         
-        download(response.url, function(data) {
+        download(url, function(data) {
           fs.writeFileSync(
-            "./dist/" + response.url
-              .replace("http://localhost:9090/", "")
-              .replace(url, ""), 
+            "./dist/" + url.replace(host, ""), 
             data
           )
-          console.log("saved: " + response.url)
+          console.log("saved: " + url)
         }) 
       }
     })
-    .goto(url + hash)
-    .evaluate(function() {
-      return "<!DOCTYPE html>\n" + document.documentElement.outerHTML
-    }, function(data) {
-      fs.writeFileSync("./dist/index.html", data)
+
+  var scraper = function() {
+    return {
+      html: "<!DOCTYPE html>\n" + document.documentElement.outerHTML,
+      links: Array.prototype 
+        .map.call(
+          document.querySelectorAll("a"), 
+          function(x) {return x.href})
+    }
+  }
+  
+  var scraperCallback = function(data) {
+    data.html && fs.writeFileSync("./dist/" + "index.html", data.html)
+    data.links && data.links
+    .filter(function(x) {return has(x, hostname)})
+    .forEach(function(x) {
+      if (!has(x, visited)) {
+        visited.push(x) 
+        nightmare.goto(x).evaluate(scraper, scraperCallback)
+      }
     })
+  } 
+
+  nightmare
+    .goto(href)
+    .evaluate(scraper, scraperCallback)
     .run(function(err, n) {
-      console.log("done")
+      console.log("done building")
       cb()
     })
 }
 
 
 function download(url, cb) {
+  var data = ""
 
-  var data = "";
-  var request = http.get(url, function(res) {
-
-    res.on('data', function(chunk) {
-      data += chunk;
-    });
-
-    res.on('end', function() {
-      cb(data);
-    })
-  });
-
-  request.on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
+  http.get(url, function(res) {
+    res.on("data", function(chunk) {data += chunk})
+    res.on("end", function() {cb(data)})
+  }).on("error", function(e) {console.log("Error: " + e.message)})
 }
